9c6a3c20633a3f1a03a8bd8af155a01ff00e10d8
adrian-albanes <fantabulosoadriangmail.com>
2022-10-18 01:36:58 -0300

    commit

---
+++ src/main/java/com/probando/mavenproject1/Principal.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.probando.mavenproject1;
+
+/**
+ *
+ * @author Admn
+ */
+public class Principal {
+    public static void main(String[] args) {
+        Mavenproject1 mp = new Mavenproject1();
+        
+        mp.setVisible(true);
+    }
+}
